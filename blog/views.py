826cdbc7b48717e442cc273e2cfc396a60c04421
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2023-10-14 01:33:29 +0300

    changed dummy posts to real posts created in shell, added posts category in admin, etc

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 21, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2023'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
